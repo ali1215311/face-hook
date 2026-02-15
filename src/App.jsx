7cd017ed1863cb6425c979214b611b322b7aa3ad
Mohammad Ali <mohammad.ali1215311@gmail.com>
2026-02-15 14:40:31 +0600

    Project FaceHook initialize

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <div className="bg-lwsGreen text-lighterDark container">
+        <button disabled className="btn-primary">
+          App
+        </button>
+      </div>
+    </>
+  );
+};
+export default App;
