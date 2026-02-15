73be5c214801c5378c4601260c7bc328e643afbc
Mohammad Ali <mohammad.ali1215311@gmail.com>
2026-02-15 15:33:35 +0600

    Add basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegistrationPage from "./pages/RegistrationPage";
+
 const App = () => {
   return (
     <>
-      <div className="bg-lwsGreen text-lighterDark container">
-        <button disabled className="btn-primary">
-          App
-        </button>
-      </div>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="me" element={<ProfilePage />} />
+        <Route path="register" element={<RegistrationPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 };
